1c461f04ee1d506d09957da084cf4a6e402124da
RoboCats <60160962+41robocats98@users.noreply.github.com>
2025-03-08 19:05:11 -0600

    Added better positions for the manipulator rotations

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -32,7 +32,7 @@ public class ElevatorSubsystem extends SubsystemBase {
     elevatorConfig.closedLoop
           .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
           .pid(0.02,0.0,0.03)
-          .outputRange(-1, 1);
+          .outputRange(-0.4, 0.4);
 
     elevatorMotor.configure(elevatorConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
   }
