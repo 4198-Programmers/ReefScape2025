f9c9d80f59bb70509987e819cf2d62edd846d66f
RoboCats <60160962+41robocats98@users.noreply.github.com>
2025-03-28 16:20:27 -0500

    updated swerve offset
    Co-Authored-By: Joe Pauly <150485779+JoePauly4198@users.noreply.github.com>

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -80,7 +80,7 @@ public final class Constants {
   public static final int FRONT_LEFT_DRIVE_MOTOR_ID = 1;
   public static final int FRONT_LEFT_ANGLE_MOTOR_ID = 2;
   public static final int FRONT_LEFT_CANCODER_ID = 13;
-  public static final double FRONT_LEFT_ANGLE_OFFSET = 0.078857421875;
+  public static final double FRONT_LEFT_ANGLE_OFFSET = 0.078857421875 - 0.5;
   public static final int FRONT_LEFT_MODULE_NUMBER = 0;
   public static final boolean FRONT_LEFT_DRIVE_INVERT = true;
   public static final boolean FRONT_LEFT_ANGLE_INVERT = false;
